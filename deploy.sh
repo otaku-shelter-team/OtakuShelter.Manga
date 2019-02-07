d30f54074398913cdb3d6637b6e4ba5a633170f5
Sergey <sergeyshaykhullin@gmail.com>
2019-02-07 15:10:26 +0300

    1.0.3 test sshpass

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 PREVIOUS_VERSION=`expr $TRAVIS_BUILD_NUMBER - 1`
 
-echo docker stop otakushelter/otaku-shelter.manga:1.0.$PREVIOUS_VERSION
-echo docker pull otakushelter/otaku-shelter.manga:1.0.$TRAVIS_BUILD_NUMBER
-echo docker run -d otakushelter/otaku-shelter.manga:1.0.$TRAVIS_BUILD_NUMBER
+docker stop otakushelter/otaku-shelter.manga:1.0.$PREVIOUS_VERSION
+docker pull otakushelter/otaku-shelter.manga:1.0.$TRAVIS_BUILD_NUMBER
+docker run -d otakushelter/otaku-shelter.manga:1.0.$TRAVIS_BUILD_NUMBER
