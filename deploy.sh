6fa2b3c67d45f45a53e5bcea6f823cc17d86f2e2
Sergey <sergeyshaykhullin@gmail.com>
2019-02-07 15:25:42 +0300

    1.0.3 test pipeline

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 PREVIOUS_VERSION=`expr $TRAVIS_BUILD_NUMBER - 1`
 
-echo docker stop otakushelter/otaku-shelter.manga:1.0.$PREVIOUS_VERSION
 echo docker pull otakushelter/otaku-shelter.manga:1.0.$TRAVIS_BUILD_NUMBER
+echo docker stop otakushelter/otaku-shelter.manga:1.0.$PREVIOUS_VERSION
+echo docker rmi otakushelter/otaku-shelter.manga:1.0.$PREVIOUS_VERSION -f 
 echo docker run -d otakushelter/otaku-shelter.manga:1.0.$TRAVIS_BUILD_NUMBER
