d87c9b4bcd2244a8e30195ff67d6bc72f50be2fd
Sergey <sergeyshaykhullin@gmail.com>
2019-02-08 14:01:49 +0300

    1.0.3 Fix docker port mapping

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ PREVIOUS_VERSION=`expr $TRAVIS_BUILD_NUMBER - 1`
 echo docker pull otakushelter/otaku-shelter.manga:1.0.$TRAVIS_BUILD_NUMBER
 echo docker stop otakushelter/otaku-shelter.manga:1.0.$PREVIOUS_VERSION
 echo docker rmi otakushelter/otaku-shelter.manga:1.0.$PREVIOUS_VERSION -f 
-echo docker run -d otakushelter/otaku-shelter.manga:1.0.$TRAVIS_BUILD_NUMBER
+echo docker run -d -p 80:80 otakushelter/otaku-shelter.manga:1.0.$TRAVIS_BUILD_NUMBER
