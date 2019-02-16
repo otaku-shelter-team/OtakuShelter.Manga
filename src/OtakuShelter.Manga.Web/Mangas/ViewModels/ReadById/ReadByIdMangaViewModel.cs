f4ac437a335f91cc0af3591c08f6aa92fcf3c256
Sergey <sergeyshaykhullin@gmail.com>
2019-02-16 17:42:48 +0300

    1.0.3 Remove from ReadByIdMangaViewModel tags, translators and authors

--- src/OtakuShelter.Manga.Web/Mangas/ViewModels/ReadById/ReadByIdMangaViewModel.cs
+++ src/OtakuShelter.Manga.Web/Mangas/ViewModels/ReadById/ReadByIdMangaViewModel.cs
@@ -22,15 +22,6 @@ namespace OtakuShelter.Manga
 		[DataMember(Name = "type")]
 		public ReadTypeItemViewModel Type { get; private set; }
 		
-		[DataMember(Name = "tags")]
-		public ICollection<ReadTagItemViewModel> Tags { get; private set; }
-
-		[DataMember(Name = "translators")]
-		public ICollection<ReadTranslatorItemViewModel> Translators { get; private set; }
-		
-		[DataMember(Name = "authors")]
-		public ICollection<ReadAuthorItemViewModel> Authors { get; private set; }
-		
 		public async Task Load(MangaContext context, int mangaId)
 		{
 			var manga = await context.Mangas.FirstAsync(m => m.Id == mangaId);
@@ -39,9 +30,6 @@ namespace OtakuShelter.Manga
 			Description = manga.Description;
 			Image = manga.Image;
 			Type = new ReadTypeItemViewModel(manga.Type);
-			Tags = manga.Tags.Select(mt => new ReadTagItemViewModel(mt.Tag)).ToList();
-			Translators = manga.Translators.Select(mt => new ReadTranslatorItemViewModel(mt.Translator)).ToList();
-			Authors = manga.Authors.Select(ma => new ReadAuthorItemViewModel(ma.Author)).ToList();
 		}
 	}
 }
