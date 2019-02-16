3825bede437495f99c46dc0f1a9ad04e914f3ecd
Sergey <sergeyshaykhullin@gmail.com>
2019-02-16 16:37:10 +0300

    1.0.3 Rename docker repository

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 PREVIOUS_VERSION=`expr $TRAVIS_BUILD_NUMBER - 1`
 
-echo docker pull otakushelter/otaku-shelter.manga:1.0.$TRAVIS_BUILD_NUMBER
-echo docker rm \$\(docker stop \$\(docker ps -a -q --filter ancestor=otakushelter/otaku-shelter.manga:1.0.$PREVIOUS_VERSION --format="{{.ID}}"\)\)
-echo docker rmi otakushelter/otaku-shelter.manga:1.0.$PREVIOUS_VERSION -f 
-echo docker run -d -p 127.0.0.1:4001:80 otakushelter/otaku-shelter.manga:1.0.$TRAVIS_BUILD_NUMBER
+echo docker pull otakushelter/manga:1.0.$TRAVIS_BUILD_NUMBER
+echo docker rm \$\(docker stop \$\(docker ps -a -q --filter ancestor=otakushelter/manga:1.0.$PREVIOUS_VERSION --format="{{.ID}}"\)\)
+echo docker rmi otakushelter/manga:1.0.$PREVIOUS_VERSION -f 
+echo docker run -d -p 127.0.0.1:4001:80 otakushelter/manga:1.0.$TRAVIS_BUILD_NUMBER
