d952eb76f8f186a0ebbfc26b8734767aa7ff73fb
Sergey <sergeyshaykhullin@gmail.com>
2019-02-23 00:35:31 +0300

    1.0.0 New deploy pipeline

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
-PREVIOUS_VERSION=`expr $TRAVIS_BUILD_NUMBER - 1`
-
-echo docker pull otakushelter/manga:1.0.$TRAVIS_BUILD_NUMBER
-echo docker rm \$\(docker stop \$\(docker ps -a -q --filter ancestor=otakushelter/manga:1.0.$PREVIOUS_VERSION --format="{{.ID}}"\)\)
-echo docker rmi otakushelter/manga:1.0.$PREVIOUS_VERSION -f 
-echo docker run -d -p 127.0.0.1:4001:80 otakushelter/manga:1.0.$TRAVIS_BUILD_NUMBER
+echo "cd /root/OtakuShelter.Infrastructure/src && \
+ansible-playbook deploy.yml \
+-e \"\
+otakushelter_hosts=mangas \
+otakushelter_container=otakushelter_manga \
+otakushelter_image=otakushelter/manga \
+otakushelter_port=4001 \
+otakushelter_build_number=$TRAVIS_BUILD_NUMBER\" \
+-i inventories/staging"
