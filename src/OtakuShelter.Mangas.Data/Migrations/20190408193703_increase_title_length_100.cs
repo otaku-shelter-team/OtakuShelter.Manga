ce8e7d3cbbc955c8a130a0b090ee06fc210e9329
NoiRTvT <Might_control437>
2019-04-08 22:37:24 +0300

    fix MangaConfiguration title length

---
+++ src/OtakuShelter.Mangas.Data/Migrations/20190408193703_increase_title_length_100.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OtakuShelter.Mangas.Migrations
+{
+    public partial class increase_title_length_100 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "title",
+                table: "mangas",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "title",
+                table: "mangas",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+        }
+    }
+}
